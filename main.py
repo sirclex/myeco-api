3ec9cfdf1fded3311c67b6eec32d2d7ff09a31aa
sirclex <lethanhluan2000@gmail.com>
2024-10-28 21:30:39 +0700

    feat: add token authorization

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
+import os
+
 from datetime import datetime
 from typing import Union, List
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 
-origins = {
-    "http://localhost:3000"
-}
+from fastapi import FastAPI, Security, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security.api_key import APIKeyHeader
+
+load_dotenv()
+
+origins = {os.getenv("CORS_ORIGIN")}
 
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_methods=["*"],
-    allow_headers=["*"]
+    CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"]
 )
+
+api_key_header = APIKeyHeader(name=os.getenv("API_KEY_NAME"), auto_error=False)
+
+async def get_api_key(api_key_header: str = Security(api_key_header)):
+    if api_key_header == f"Bearer {os.getenv("API_KEY")}":
+        return api_key_header
+    else:
+        raise HTTPException(status_code=403, detail="Could not validate credentials")
 
 from models.wallet import WalletModel
 from models.identity import IdentityModel
@@ -30,126 +40,154 @@ from utils import category_logic
 from utils import subcategory_logic
 
 from utils.initialize_database import initialize_database, engine
+
 # from sqlalchemy.orm import Session
 
 # from sqlalchemy import select
 
 initialize_database()
 
+
 # Wallet
 @app.post("/wallet/create")
-async def create_wallet(wallet: WalletModel):
+async def create_wallet(wallet: WalletModel, api_key: str = Security(get_api_key)):
     return wallet_logic.create_wallet(wallet=wallet, engine=engine)
 
+
 @app.get("/wallets")
-async def get_wallets():
+async def get_wallets(api_key: str = Security(get_api_key)):
     return wallet_logic.get_all_wallets(engine)
 
+
 @app.get("/wallet")
-async def get_wallet(id: int):
+async def get_wallet(id: int, api_key: str = Security(get_api_key)):
     return wallet_logic.get_wallet(id, engine)
 
+
 @app.post("/wallet/delete")
-async def delete_wallet(id: int):
+async def delete_wallet(id: int, api_key: str = Security(get_api_key)):
     return wallet_logic.delete_wallet(id, engine)
 
+
 @app.post("/wallet/revive")
-async def delete_wallet(id: int):
+async def delete_wallet(id: int, api_key: str = Security(get_api_key)):
     return wallet_logic.revive_wallet(id, engine)
 
+
 @app.post("/wallet/update")
-async def update_wallet(wallet: WalletModel):
+async def update_wallet(wallet: WalletModel, api_key: str = Security(get_api_key)):
     return wallet_logic.update_wallet(wallet, engine)
 
-#Identity
+
+# Identity
 @app.post("/identity/create")
-async def create_identity(identity: IdentityModel):
+async def create_identity(identity: IdentityModel, api_key: str = Security(get_api_key)):
     return identity_logic.create_identity(identity, engine)
 
+
 @app.get("/identities")
-async def get_identities():
+async def get_identities(api_key: str = Security(get_api_key)):
     return identity_logic.get_all_identities(engine)
 
+
 @app.get("/identity")
-async def get_identity(id: int):
+async def get_identity(id: int, api_key: str = Security(get_api_key)):
     return identity_logic.get_identity(id, engine)
 
+
 @app.post("/identity/delete")
-async def delete_identity(id: int):
+async def delete_identity(id: int, api_key: str = Security(get_api_key)):
     return identity_logic.delete_identity(id, engine)
 
+
 @app.post("/identity/revive")
-async def revive_identity(id: int):
+async def revive_identity(id: int, api_key: str = Security(get_api_key)):
     return identity_logic.revive_identity(id, engine)
 
+
 @app.post("/identity/update")
-async def update_identity(identity: IdentityModel):
+async def update_identity(identity: IdentityModel, api_key: str = Security(get_api_key)):
     return identity_logic.update_identity(identity, engine)
+
 
 # TODO Transaction Category
 @app.get("/categories")
-async def get_categories():
+async def get_categories(api_key: str = Security(get_api_key)):
     return category_logic.get_all_categories(engine)
+
 
 # TODO Transaction Subcategory
 # @app.get("/allsubcategories")
 # async def get_subcategories():
 #     return subcategory_logic.get_all_subcategories(engine)
 
+
 @app.get("/subcategories")
-async def get_subcategories(category_id: int):
+async def get_subcategories(category_id: int, api_key: str = Security(get_api_key)):
     return subcategory_logic.get_subcategories(engine, category_id)
+
 
 # Transaction
 @app.post("/transaction/create")
-async def create_transaction(transaction: TransactionModel, debts: List[DebtModel]):
+async def create_transaction(transaction: TransactionModel, debts: List[DebtModel], api_key: str = Security(get_api_key)):
     transaction_id = transaction_logic.create_transaction(transaction, engine)
     if len(debts) > 0:
         for debt in debts:
             debt.transaction_id = transaction_id
         debt_logic.create_multiple_debt(debts=debts, engine=engine)
 
+
 @app.get("/transactions")
-async def get_transactions():
+async def get_transactions(api_key: str = Security(get_api_key)):
     return transaction_logic.get_all_transactions(engine)
 
+
 @app.get("/transaction")
-async def get_transaction(id: int):
+async def get_transaction(id: int, api_key: str = Security(get_api_key)):
     return transaction_logic.get_transaction(id, engine)
 
+
 @app.post("/transaction/delete")
-async def delete_transaction(id: int):
+async def delete_transaction(id: int, api_key: str = Security(get_api_key)):
     return transaction_logic.delete_transaction(id, engine)
 
+
 @app.post("/transaction/revive")
-async def revive_transaction(id: int):
+async def revive_transaction(id: int, api_key: str = Security(get_api_key)):
     return transaction_logic.revive_transaction(id, engine)
 
+
 @app.post("/transaction/update")
-async def update_transaction(transaction: TransactionModel):
+async def update_transaction(transaction: TransactionModel, api_key: str = Security(get_api_key)):
     return transaction_logic.update_transaction(transaction, engine)
+
 
 # Debt
 @app.post("/debt/create")
-async def create_debt(debt: DebtModel):
+async def create_debt(debt: DebtModel, api_key: str = Security(get_api_key)):
     return debt_logic.create_debt(debt, engine)
 
+
 @app.get("/debts")
-async def get_debts():
+async def get_debts(api_key: str = Security(get_api_key)):
     return debt_logic.get_all_debts(engine)
 
+
 @app.get("/debt")
-async def get_debt(id: int):
+async def get_debt(id: int, api_key: str = Security(get_api_key)):
     return debt_logic.get_debt(id, engine)
 
+
 @app.post("/debt/delete")
-async def delete_debt(id: int):
+async def delete_debt(id: int, api_key: str = Security(get_api_key)):
     return debt_logic.delete_debt(id, engine)
 
+
 @app.post("/debt/revive")
-async def revive_debt(id: int):
+async def revive_debt(id: int, api_key: str = Security(get_api_key)):
     return debt_logic.revive_debt(id, engine)
 
+
 @app.post("/debt/update")
-async def update_debt(debt: DebtModel):
+async def update_debt(debt: DebtModel, api_key: str = Security(get_api_key)):
     return debt_logic.update_debt(debt, engine)
