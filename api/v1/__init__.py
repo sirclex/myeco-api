ca40231419508176f7cdae8c52b142a65b2805c2
sirclex <lethanhluan2000@gmail.com>
2024-12-22 21:18:17 +0700

    fix: no endpoint

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from api.v1 import wallets, identity_types, identities
+from api.v1 import wallets, identity_types, identities, categories
 
 api_router = APIRouter()
 api_router.include_router(wallets.router, prefix="/wallet", tags=["wallets"])
 api_router.include_router(identity_types.router, prefix="/identityType", tags=["identity_types"])
 api_router.include_router(identities.router, prefix="/identity", tags=["identities"])
+api_router.include_router(categories.router, prefix="/category", tags=["categories"])
