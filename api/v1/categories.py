5e0db0b544ed729f3935c18f9c85e1896428b07e
sirclex <lethanhluan2000@gmail.com>
2024-12-22 22:38:56 +0700

    fix: method not found

--- api/v1/categories.py
+++ api/v1/categories.py
@@ -12,16 +12,16 @@ router = APIRouter()
 
 @router.get("", response_model=List[CategoryResponse])
 def get_all_categories(db: Session = Depends(get_db)) -> Any:
-    return category_logic.get_all_identity_types(db)
+    return category_logic.get_all_categories(db)
 
 @router.post("", response_model=CategoryResponse)
 def create_category(*, db: Session = Depends(get_db), wallet_in: CategoryCreate) -> Any:
-    return category_logic.create_identity_type(db, wallet_in)
+    return category_logic.create_category(db, wallet_in)
 
 @router.put("", response_model=CategoryResponse)
 def update_category(*, db: Session = Depends(get_db), wallet_in: CategoryUpdate) -> Any:
-    return category_logic.update_identity_type(db, wallet_in)
+    return category_logic.update_category(db, wallet_in)
 
 @router.delete("", response_model=Message)
 def delete_category(*, db: Session = Depends(get_db), id: int) -> Any:
-    return category_logic.delete_identity_type(db, id)
+    return category_logic.delete_category(db, id)
